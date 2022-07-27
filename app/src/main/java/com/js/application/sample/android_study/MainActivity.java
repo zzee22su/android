55dd559ef83679b38d39e20ee9bf9f5cd991bd00
zzee22su <patrickstar0910@gmail.com>
2022-07-27 17:16:26 +0900

    ADD : 버튼 클릭 시, ON/OFF text 바뀌도록 설정

--- app/src/main/java/com/js/application/sample/android_study/MainActivity.java
+++ app/src/main/java/com/js/application/sample/android_study/MainActivity.java
@@ -3,12 +3,33 @@ package com.js.application.sample.android_study;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnOnOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnOnOff = findViewById(R.id.btnOnOff);
+
+        mBtnOnOff.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnOnOff:
+                if(mBtnOnOff.getText()=="ON") {
+                    mBtnOnOff.setText("OFF");
+                } else {
+                    mBtnOnOff.setText("ON");
+                }
+                break;
+        }
     }
 }
